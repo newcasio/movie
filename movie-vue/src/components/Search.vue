831e255903cd6cabd7bb363a4bb56d251ab3579f
Brad <braddong@hotmail.com>
2019-06-03 14:35:27 +1000

    next/prev page, movie image

--- movie-vue/src/components/Search.vue
+++ movie-vue/src/components/Search.vue
@@ -6,13 +6,16 @@
     </form>
     <button @click="searchDb">Submit</button>
     <!-- <p>{{info}}</p> -->
-    <ul v-for="movie in info" :key="movie.id">
+    <ul v-for="movie in listOfMovies" :key="movie.id">
       <li>
-        <h3>{{movie.original_title}}</h3>
+        <h3 :noPage="lastPage" :firstPage="firstPage">{{movie.original_title}}</h3>
+        <img :src="`http://image.tmdb.org/t/p/w185/${movie.poster_path}`">
         <p>Overview: {{movie.overview}}</p>
         <p>Viewer rating: {{movie.vote_average}}</p>
       </li>
     </ul>
+    <button v-if="prevPage" @click="gotoPrevPage">Previous page</button>
+    <button v-if="nextPage" @click="gotoNextPage">Next page</button>
   </div>
 </template>
 
@@ -23,15 +26,46 @@ export default {
   data() {
     return {
       searchString: "",
-      info: null
+      info: null,
+      resultsPage: 1,
+      nextPage: false,
+      prevPage: false,
+      listOfMovies: null
     };
   },
   methods: {
     searchDb() {
-      let searchURL = `https://api.themoviedb.org/3/search/movie?api_key=***KEY***&query=${
+      this.nextPage = true;
+      let searchURL = `https://api.themoviedb.org/3/search/movie?api_key=*****INSERT_KEY*****&query=${
         this.searchString
-      }&page=1`;
-      axios.get(searchURL).then(res => (this.info = res.data.results));
+      }&page=${this.resultsPage}`;
+      axios.get(searchURL).then(res => {
+        this.info = res.data;
+        this.listOfMovies = res.data.results;
+      });
+    },
+    gotoNextPage() {
+      this.resultsPage++;
+      this.searchDb();
+    },
+    gotoPrevPage() {
+      this.resultsPage--;
+      this.searchDb();
+    }
+  },
+  computed: {
+    totalPages() {
+      return this.info.total_pages;
+    },
+    lastPage() {
+      if (this.resultsPage === this.totalPages) {
+        this.nextPage = false;
+      }
+    },
+    firstPage() {
+      if (this.resultsPage !== 1) {
+        this.prevPage = true;
+      }
     }
   }
 };
